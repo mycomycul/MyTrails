001ba0b4b57832f9787aad39bd1152d8f49e2503
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-08-21 21:22:20 -0700

    Comment out ImportJsontrail from MyTrail

--- MyTrails/Controllers/TrailController.cs
+++ MyTrails/Controllers/TrailController.cs
@@ -194,16 +194,16 @@ namespace MyTrails.Controllers
             return RedirectToAction("Index");
         }
 
-        public void ImportJSONtrail()
-        {
-            using(StreamReader r = new StreamReader("C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"))
-            {
-                var trailJson = r.ReadToEnd();
-                JavaScriptSerializer js = new JavaScriptSerializer();
-                dynamic trail = js.Deserialize<dynamic>(trailJson);
-            }
-            Console.Read();
-        }
+        //public void ImportJSONtrail()
+        //{
+        //    using(StreamReader r = new StreamReader("C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"))
+        //    {
+        //        var trailJson = r.ReadToEnd();
+        //        JavaScriptSerializer js = new JavaScriptSerializer();
+        //        dynamic trail = js.Deserialize<dynamic>(trailJson);
+        //    }
+        //    Console.Read();
+        //}
 
 
 
