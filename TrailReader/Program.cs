ea9af9be88511c15771c46cce6e8f19e2720b62f
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-08-22 11:53:59 -0700

    Save trail values to class

--- TrailReader/Program.cs
+++ TrailReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TrailReader
 {
@@ -15,75 +17,67 @@ namespace TrailReader
         static void Main(string[] args)
         {
 
-            using (StreamReader r = new StreamReader(@"C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"))
+            //var t = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"));
+            var t = JObject.Parse(File.ReadAllText(@"C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"));
+            IList<JToken> results = t["features"].Children().ToList();
+            IList<Trail> trails = new List<Trail>();
+            foreach (JToken item in results)
             {
-                var trailJson = r.ReadToEnd();
-                /* read JSON with using System.Web.Script.Serialization;*/
-                //   JavaScriptSerializer js = new JavaScriptSerializer();
-                //    dynamic trail = js.Deserialize<dynamic>(trailJson);
-                //dynamic trailStore = new ExpandoObject();
-
-
-                //    trailStore.fields = new List<string>();
-                //    for (int i = 0; i < trail["fields"].Length; i++)
-                //    {
-                //        trailStore.fields.Add(trail["fields"][i]["name"]);
-                //    }
-
-                //    foreach (var item in trail["features"])
-                //    {
-                //        var t = item["attributes"];
-                //        foreach (var attribute in t)
-                //        {
-                //            var k = attribute.Key;
-                //            var z = attribute.Value;
-                //            Console.WriteLine(k + " = " + z);
-                //        }
-                //    }
-
-                dynamic trail = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(@"C:\Users\Michael\Desktop\Programming\Projects\OlympicTrailData.Json"));
-                //dynamic trailStore = new ExpandoObject();
-                var trailStore = new List<Trail>();
-                var trailFeatures = new List<string>();
-
-                for (int i = 0; i < trail["fields"].Count; i++)
-                {
-                    trailFeatures.Add((string)trail["fields"][i]["name"]);
-                }
-                    foreach (var item in trail["features"])
-                    {
-
-                        Trail newTrail = new Trail();
-                        newTrail.Id = item["attributes"]["OBJECTID"];
-                        newTrail.TrailName = item["attributes"]["TRLNAME"];
-                        newTrail.Status = item["attributes"]["TRLSTATUS"];
-                        newTrail.Notes = item["attributes"]["NOTES"];
-
-                        trailStore.Add(newTrail);
-                    }
-                
-
-
-
-
-
-
-                Console.Read();
-
+                Trail trail = item["attributes"].ToObject<Trail>();
+                trail.GEOMETRY = item["geometry"]["paths"][0];
+                trails.Add(trail);
+               // Console.Read();
             }
+            //dynamic trailStore = new ExpandoObject();
+            //Trail trail = (Trail)t.
 
+            //var trailStore = new List<Trail>();
+            //var trailFeatures = ((Array)(trail.fields));
+            Console.Read();
+
+            //for (int i = 0; i < trailFeatures.Length; i++)
+            //{
+
+            //    Console.Read();
+            //}
+            //foreach (var item in trail["features"])
+            //{
+
+            //    Trail newTrail = new Trail();
+            //    //newTrail.Id = item["attributes"]["OBJECTID"];
+            //    newTrail.TrailName = item["attributes"]["TRLNAME"];
+            //    newTrail.Status = item["attributes"]["TRLSTATUS"];
+            //    newTrail.Notes = item["attributes"]["NOTES"];
+
+            //    trailStore.Add(newTrail);
+            //}
+
+
+            Console.Read();
 
         }
 
 
-        public class Trail
-        {
-            public int Id { get; set; }
-            public string TrailName { get; set; }
-            public string Status { get; set; }
-            public string Notes { get; set; }
+    }
 
-        }
+
+    public class Trail
+    {
+        //public object displayFieldName { get; set; }
+        //public object fieldAliases { get; set; }
+        //public object features { get; set; }
+        //public object fields { get; set; }
+        //public object geometryType { get; set; }
+
+        //public object spatialReference { get; set; }
+
+        public string TRLNAME { get; set; }
+        public string TRLSTATUS { get; set; }
+        public string NOTES { get; set; }
+        public string UNITCODE { get; set; }
+        public object GEOMETRY { get; set; }
+
     }
 }
+
 
