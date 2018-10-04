ffe88ab53ad7b8785fd96ebfe3147ffe6c2aa8a1
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-10-03 23:48:13 -0700

    Move combine scritps to trailcombine.js

---
+++ MyTrails/Scripts/TrailCombine.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    $('.trail').on("click", function (event) {
+        $('.trail').removeClass("selected-list-item");
+        $(this).addClass("selected-list-item");
+    })
+    $('.li').on("click", function (event) {
+        $(this).toggleClass("selected-list-item");
+    })
+});
+
+function submitCombine() {
+    var trailsections = $("li.li.selected-list-item").map(function () {
+        return $(this).text();
+    }).get();
+    var trail = $('li.trail.selected-list-item').text();
+    var data = {
+        trail: trail,
+        trailsections: trailsections
+    };
+
+
+    $.post({
+        url: "Combine",
+        data: data,
+        success: function (result) {
+            alert(result);
+        }
+    });
+    return true;
+}
