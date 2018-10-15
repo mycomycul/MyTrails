fdded9c85531ce95e98eebd1c6e55ab0ee2b8cb0
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-10-14 22:18:33 -0700

    Unselecting trail name removes it form the map Does not check if data has already been retrieved when selected again and resaves as new points

--- MyTrails/Scripts/TrailCombine.js
+++ MyTrails/Scripts/TrailCombine.js
@@ -15,32 +15,50 @@
             if (selected === false) {
                 selected = true;
                 $thisSection.addClass("selected-list-item");
-                getGeoJsonData($thisSection.text());
+                getGeoJsonData($thisSection);
             }
             else {
                 selected = false;
                 $thisSection.removeClass("selected-list-item");
+                removeSection($thisSection);
             }
         });
     });
 });
 //Get Trail Section Coordinates
-function getGeoJsonData(geoDataTrailName) {
+function getGeoJsonData(geoDataTrail) {
+
     var data = {
-        trailSectionName: geoDataTrailName
+        trailSectionName: geoDataTrail.text()
     };
     $.ajax({
         url: "GetGeoJsonData",
         data: data,
         dataType: 'json',
         success: function (result) {
-            for (var k = 0; k < result.length; k++) {
-                let newFeature = CreateGoogleDataFromArray(result[0].Geometry[0]);
-                addDataToMap(newFeature, result[k].Notes);
+            if (result.length > 0) {
+                for (var k = 0; k < result.length; k++) {
+                    let newFeature = CreateGoogleDataFromArray(result[k].Geometry[0]);
+                    let mapFeature = addDataToMap(newFeature, result[k].Notes);
+                    mapPoints.push(mapFeature);
+                    let featureNumbers = geoDataTrail.data("marker");
+                    featureNumbers.push(mapPoints.length - 1);
+                    geoDataTrail.data("marker",featureNumbers);
+                }
+            }
+            else {
+                alert("No valid map data was available");
             }
         }
     });
     return true;
+}
+//Remove Trail Section
+function removeSection(sectionToRemove) {
+    let sections = sectionToRemove.data("marker");
+    for (var i = 0; i < sections.length; i++) {
+        mapPoints[sections[i]].setMap(null);
+    }
 }
 //Submit selected trail from geodata and selected trails from GEOJson Data to be combined and saved in the the database
 function submitCombine() {
@@ -82,8 +100,7 @@ function addDataToMap(googleCoordinates, title) {
             path: googleCoordinates,
             strokeColor: "#00ff00",
             strokeOpacity: 0.8,
-            strokeWeight: 8,
-            title: title
+            strokeWeight: 8
         });
         mapFeature.addListener('click', function () {
             map.setZoom(10);
@@ -92,6 +109,8 @@ function addDataToMap(googleCoordinates, title) {
 
         });
     }
+
+    
     //Else if only one coordinate was received, create a Google marker
     else {
         mapFeature = new google.maps.Marker({
@@ -110,7 +129,7 @@ function addDataToMap(googleCoordinates, title) {
     }    
     mapFeature.setMap(map);
 
-    mapPoints.push(mapFeature);
+    return mapFeature; 
 }
 
 
