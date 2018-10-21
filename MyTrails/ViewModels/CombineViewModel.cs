6bb8e9b28c911a0745d0f85ec72219d12591c1cd
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-10-21 15:39:49 -0700

    Add Construcotr and summary

--- MyTrails/ViewModels/CombineViewModel.cs
+++ MyTrails/ViewModels/CombineViewModel.cs
@@ -6,9 +6,22 @@ using System.Web;
 
 namespace MyTrails.ViewModels
 {
+
+    /// <summary>
+    /// View Model for passing names of trails in the DB and names of trial in Json data
+    /// </summary>
     public class CombineViewModel
     {
         public List<string> Trails { get; set; }
         public List<string> TrailSections { get; set; }
+
+        public CombineViewModel()
+        {
+        }
+        public CombineViewModel(List<string> trails, List<string> trailSections)
+        {
+            Trails = trails;
+            TrailSections = trailSections;
+        }
     }
 }
