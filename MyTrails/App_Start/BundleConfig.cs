427cb8a793160966c43ac861d774a40616f32c9e
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-10-27 11:21:36 -0700

    Add Map to conditions index and spilt up styling in Site.css

--- MyTrails/App_Start/BundleConfig.cs
+++ MyTrails/App_Start/BundleConfig.cs
@@ -18,6 +18,10 @@ namespace MyTrails
                                     "~/Scripts/trailcombine.js"));
 
 
+            bundles.Add(new ScriptBundle("~/bundles/map").Include(
+                                    "~/Scripts/map.js"));
+
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
