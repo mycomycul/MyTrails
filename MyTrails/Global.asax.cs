93fc2298e470393e368df7f6c02f165974777772
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-02-13 00:35:38 -0800

    Add API Handlers

--- MyTrails/Global.asax.cs
+++ MyTrails/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.SqlServer;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -18,10 +14,13 @@ namespace MyTrails
             
             //SqlProviderServices.SqlServerTypesAssemblyName = "Microsoft.SqlServer.Types, Version=14.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91";
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
+            
+
         }
     }
 }
