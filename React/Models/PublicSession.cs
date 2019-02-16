e3c53ebd1d77dbc52fd8a60cf40c026eb72c9340
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-02-16 00:33:35 -0800

    Add React Project

---
+++ React/Models/PublicSession.cs
@@ -0,0 +1,11 @@
+﻿namespace React.Models
+{
+    /// <summary>
+    /// Represents public session of the web application
+    /// that can be shared in browser's window object.
+    /// </summary>
+    public class PublicSession
+    {
+        public ServiceUser ServiceUser { get; set; }
+    }
+}
