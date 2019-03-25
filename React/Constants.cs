850d1b9d0c752e52b4ae1d00d1001876deae94d2
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-03-24 22:05:19 -0700

    Clean MyTrails

--- React/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace React
-{
-    public static class Constants
-    {
-        public static string AuthorizationCookieKey => "Auth";
-        public static string HttpContextServiceUserItemKey => "ServiceUser";
-    }
-}
